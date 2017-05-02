7519f055f4fd9fc9cf5ea3af0c3584169c4cd191
brandon <bcooper085@gmail.com>
2017-05-02 16:53:18 -0700

    add moc database with controller and view testing

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+    }
+}
